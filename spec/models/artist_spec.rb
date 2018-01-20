1a4ec5c88cdc32b546c2d65bd4ef4a77daed3fbb
Erlemorgaine <erle_3@hotmail.com>
2018-01-20 22:36:43 +0100

    Testing all Model validations with ShouldaMatchers

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,35 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   describe "validations" do
-    it "is invalid without name" do
-
-      artist = Artist.new(name: "")
-      artist.valid?
-      expect(artist.errors).to have_key(:name)
-    end
-
-    it "is invalid without birth_date" do
-      artist = Artist.new(birth_date: nil)
-      artist.valid?
-      expect(artist.errors).to have_key(:birth_date)
-    end
-
-    it "is invalid without origin" do
-      artist = Artist.new(origin: "")
-      artist.valid?
-      expect(artist.errors).to have_key(:origin)
-    end
-
-    it "is invalid without genre" do
-      artist = Artist.new(genre: nil)
-      artist.valid?
-      expect(artist.errors).to have_key(:genre)
-    end
-
-    it "is invalid with description longer than 500 words" do
-      artist = Artist.new(description: "1" * 501)
-      artist.valid?
-      expect(artist.errors).to have_key(:description)
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:birth_date) }
+    it { is_expected.to validate_presence_of(:origin) }
+    it { is_expected.to validate_presence_of(:genre) }
+    it { is_expected.to validate_length_of(:description).is_at_most(500) }
   end
 end
---
+++ spec/models/song_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Song, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:year) }
+    it { is_expected.to validate_presence_of(:album) }
+  end
+end
