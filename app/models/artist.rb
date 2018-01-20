2b40f9fd4c70157885b6c401a2a3a0169a3270cc
Erlemorgaine <erle_3@hotmail.com>
2018-01-20 14:00:34 +0100

    Create Artist Showpage and add column is_band to Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,4 +6,5 @@ class Artist < ApplicationRecord
   validates :origin, presence: true
   validates :genre, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
+  validates :is_band, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120113618) do
+ActiveRecord::Schema.define(version: 20180120125640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180120113618) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.boolean "is_band"
   end
 
   create_table "songs", force: :cascade do |t|
