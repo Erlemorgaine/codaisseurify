8b5a24f3bcd6ea6052315ede7e3beed0aa7815b3
Erlemorgaine <erle_3@hotmail.com>
2018-01-20 13:24:13 +0100

    Add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'artists#home'
+
+  resources :artists do
+    resources :songs
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
