8d80602cc1a9e764d20155cc7212705b128c87ed
Erlemorgaine <erle_3@hotmail.com>
2018-01-21 12:13:12 +0100

    Configure CarrierWave to not upload in Cloudinary in test

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :photo do
-    image         { File.open(File.join(Rails.root, '/spec/features/cloud.jpg')) }
+    image         { File.open(File.join(Rails.root, '/spec/support/uploads/cloud.jpg')) }
     artist        { build(:artist) }
   end
 end
