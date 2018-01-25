cd61fa3edd9006b467f59facdf717004a90249ae
Erlemorgaine <erle_3@hotmail.com>
2018-01-25 15:26:36 +0100

    Add API songs actions

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,7 +1,27 @@
 class Api::ArtistsController < ApplicationController
-  def index
+  before_action :set_artist, only: [:show]
+
+  def show
+
+    songs = artist.songs
     render status: 200, json: {
-      artists: Artist.all
+      songs: songs
     }.to_json
+
+    photos = artist.photos
+    render status: 200, json: {
+      photos: photos
+    }.to_json
+
   end
+
+  def set_artist
+    def index
+      artist = Artist.find(params[:id])
+      render status: 200, json: {
+        artists: Artist.all
+      }.to_json
+    end
+  end
+
 end
--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,41 @@
-class Api::RoomsController < ApplicationController
-  def index
+class Api::SongsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :load_artist
+
+  def create
+    song = artist.songs.create(song_params)
+
+    if song.save
+      render status: 201, json: {
+        song: song
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+
     render status: 200, json: {
-      songs: Song.all
+      message: "Song successfully deleted"
     }.to_json
   end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :year, :album, :artist_id)
+  end
+
+  def load_artist
+    artist = Artist.find(params[:artist_id])
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+
 end
