b7747ae164a1eb186efbd3786b712ea057f4c5da
Erlemorgaine <erle_3@hotmail.com>
2018-01-25 21:52:41 +0100

    Delete one song test

--- spec/features/delete_song_spec.rb
+++ spec/features/delete_song_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 feature 'Delete song', js: true do
   let!(:artist) { create :artist }
-  let!(:song) { create :song, artist: artist }
+  let!(:song) { create :song, id: 20, name: 'Habanera', artist: artist }
 
   scenario 'delete song' do
-    visit artist_song_path(artist)
+    visit artist_path(artist)
 
-    click_link '',
+    # click_link 'delete-song-<%= song.id%>'
 
-    page.execute_script("$('form').submit()")
+    find(:xpath, "//label[contains(text(),'Habanera')]/following-sibling::a").click
 
-    expect(page).to have_content('Be Batman')
+    expect(page).to have_css("li", :count => 0)
   end
 end
