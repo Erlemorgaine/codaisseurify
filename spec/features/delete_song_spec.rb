c467cceca06d44794c7938a3c46711fcd0ae0b04
Erlemorgaine <erle_3@hotmail.com>
2018-01-25 14:11:23 +0100

    Modify feature Delete song

--- spec/features/delete_song_spec.rb
+++ spec/features/delete_song_spec.rb
@@ -5,18 +5,12 @@ feature 'Delete song', js: true do
   let!(:song) { create :song, artist: artist }
 
   scenario 'delete song' do
-    # Point your browser towards the todo path
     visit artist_song_path(artist)
 
-    # Enter description in the text field
     click_link '',
 
-    # Press enter (to submit the form)
     page.execute_script("$('form').submit()")
 
-    # Expect the new task to be displayed in the list of tasks
     expect(page).to have_content('Be Batman')
-    expect(page).to have_content('1919')
-    expect(page).to have_content('Batman')
   end
 end
