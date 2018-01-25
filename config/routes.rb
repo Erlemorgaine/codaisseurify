d55e0e150165a31ed163438f588fd1da288bc2ae
Erlemorgaine <erle_3@hotmail.com>
2018-01-25 14:29:19 +0100

    Add API artists index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   resources :artists, except: [:new, :create, :edit, :update] do
     resources :songs, only: [:new, :create, :destroy]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :artists, except: [:new, :create, :edit, :update] do
+      resources :songs, only: [:new, :create, :destroy]
+    end
+  end
+
 end
