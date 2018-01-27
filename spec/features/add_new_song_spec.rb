c7070d6b91b0e6d13dec211346c17083908456de
Erlemorgaine <erle_3@hotmail.com>
2018-01-27 22:51:30 +0100

    Try to fix Delete song test

--- spec/features/add_new_song_spec.rb
+++ spec/features/add_new_song_spec.rb
@@ -4,16 +4,17 @@ feature 'Add new song', js: true do
   let!(:artist) { create :artist }
 
   scenario 'add a new song' do
-    visit new_artist_song_path(artist)
+    visit artist_path(artist)
 
     fill_in 'song_name', with: 'Go your own way'
-    select '1919', from: 'song_year'
-    fill_in 'song_album', with: 'Batman'
+    fill_in 'song_year', with: '1990'
+    #select '1919', from: 'song_year'
+    fill_in 'song_album', with: 'Rumours'
 
     page.execute_script("$('form').submit()")
 
     expect(page).to have_content('Go your own way')
-    expect(page).to have_content('1919')
+    expect(page).to have_content('1990')
     expect(page).to have_content('Rumours')
   end
 end
