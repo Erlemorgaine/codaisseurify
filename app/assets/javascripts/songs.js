c5e749b12d77a162d4a5b1375fafd86aa5fe95b1
Erlemorgaine <erle_3@hotmail.com>
2018-01-28 17:34:55 +0100

    FINALLY managed with JSON

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,18 +1,17 @@
-//require jquery
-
-artistId = $('div[hidden]').attr('id');
-
 function nextSongId() {
   return $(".song-item").length + 1;
 };
 
 function createSong(name, year, album) {
 
+  let artistId = $('div[hidden]').attr('id');
+
   var newSong = { name: name, year: year, album: album };
 
   $.ajax({
     type: "POST",
-    url: "/api/artists/" + artistId + "/songs",
+    //url: "/api/artists/" + artistId + "/songs",
+    url: "/api/artists/1/songs",
     data: JSON.stringify({
         song: newSong
     }),
